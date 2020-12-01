7297b8e6bc432e4c3778e6135da51369b9babe80
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-01 16:18:20 +0100

    Altri elementi aggiunti

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,61 @@
-// Un alert espone 5 numeri casuali diversi.Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+/***************************************************
+CONSEGNA
+
+Un alert espone 5 numeri casuali diversi.Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+****************************************************/
 
 $(function () {
 
-var numberList = $('.casual_numbers');
-var startButton = $('#start_game');
-var casualNumbers = [];
+  var numberList = $('.casual_numbers');
+  var startButton = $('#start_game');
+  var hiddenElements = $('.hidden');
+  var casualNumbers = [];
+  var userNumbers = [];
 
-console.log(casualNumbers);
+// Scrivo una funzione che generi un numero da 1 a numero casuale.
 
-//Scrivo una funzione che generi un numero da 1 a numero casuale
-
-function randomOneTo(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
-
-//Scrivo una funzione che mi dica se un numero è presente in un array
-
-function verifyInArray(array, number) {
-   var i = 0;
-   while ( i < array.length) {
-     if (number === array [i]) {
-       return true;
-     }
-     i++;
-   }
-}
-
-//Ciclo while che inserisce 5 numeri casuali nell'array  casualNumbers a patto che questi non si ripetano
-var list = "";
-while (casualNumbers.length < 5) {
-
-  var numberComputer = randomOneTo(100);
-  var list = list + " " + numberComputer;
-
-  if (!verifyInArray(casualNumbers , numberComputer)) {
-    casualNumbers.push(numberComputer);
-    numberList.text(list);
+  function randomOneTo(max) {
+    return Math.floor(Math.random() * max) + 1;
   }
 
-}
+// Scrivo una funzione che mi dica se un numero è presente in un array.
 
-console.log(casualNumbers);
+  function verifyInArray(array, number) {
+     var i = 0;
+     while ( i < array.length) {
+       if (number === array [i]) {
+         return true;
+       }
+       i++;
+     }
+  }
 
-startButton.click(function() {
-  console.log("dollarino");
+// Ciclo while che inserisce 5 numeri casuali nell'array  casualNumbers a patto che questi non si ripetano. I numeri vengono anche stampari a schermo.
 
+  var list = "";
+  while (casualNumbers.length < 5) {
 
-});
+    var numberComputer = randomOneTo(100);
+    var list = list + " " + numberComputer;
 
+    if (!verifyInArray(casualNumbers , numberComputer)) {
+      casualNumbers.push(numberComputer);
+      numberList.text(list);
+    }
+
+  }
+
+  console.log(casualNumbers);
+// Scrivo cosa succede quando clicco su "Avvia Gioco".
+
+  startButton.click(function() {
+
+    setTimeout(game, 30000);
+
+    function game() {
+      console.log("we");
+    }
+
+  });
 
 });
